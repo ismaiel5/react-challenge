159b9fa0588b25ac53997c3024c6490565c5205e
ismaiel <ismaiel.altahan@gmail.com>
2021-06-19 18:57:59 +0800

    -update card component - update home container

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,22 @@
-import Styles from '../styles/Card.module.scss';
-import Button from "./Button"
+import Styles from "../styles/Card.module.scss";
+import Button from "./Button";
 
 const Card = (props) => {
-  return <div className={Styles.card}>
-      testing
-      <Button title="Generate"/>
-  </div>;
+  return (
+    <section className={Styles.card}>
+      <Button title="Generate 4 Objects" />
+      <br />
+      <br />
+
+      <div>
+        Link :
+        <a href="https://github.com/ismaiel5/react-challenge/commits/master">  Click to download objects</a>
+      </div>
+
+      <br />
+      <Button title="Print Report" />
+    </section>
+  );
 };
 
 export default Card;
--- containers/Home/index.js
+++ containers/Home/index.js
@@ -1,12 +1,18 @@
-import Card from "../../components/Card";
 import Styles from "../../styles/Home.module.scss";
 
+import Card from "../../components/Card";
+import ReportCard from "../../components/ReportCard";
+
 const HomePage = () => {
+  const showReport = false;
+
   return (
     <>
       <main className={Styles.container}>
         <h1>Reactjs Challenge</h1>
         <Card />
+        <br />
+        {showReport ? <ReportCard /> : ""}
       </main>
     </>
   );
