86f50d80112b3e8f483c9435201861a9676e1cba
ismaiel <ismaiel.altahan@gmail.com>
2021-06-19 21:19:44 +0800

    -added onclick function in button componenet. - added function to handle onlick in card component. -added useEffect to handle the state of showHide for the bottom section

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,7 @@
 import Styles from "../styles/Button.module.scss";
 
 const Button = (props) => {
-  return <button className={Styles.btn}>{props.title}</button>;
+  return <button className={Styles.btn} onClick={props.clickHandler}>{props.title}</button>;
 };
 
 export default Button;
--- components/Card.js
+++ components/Card.js
@@ -1,20 +1,29 @@
+import { useState } from "react";
+
 import Styles from "../styles/Card.module.scss";
 import Button from "./Button";
 
 const Card = (props) => {
+  const [showSection, setShowSection] = useState(false);
+
+  function generateObjects() {
+    setShowSection(true);
+  }
+
   return (
     <section className={Styles.card}>
-      <Button title="Generate 4 Objects" />
+      <Button title="Generate 4 Objects" clickHandler={generateObjects} />
       <br />
       <br />
 
-      <div>
-        Link :
-        <a href="https://github.com/ismaiel5/react-challenge/commits/master">  Click to download objects</a>
-      </div>
-
-      <br />
-      <Button title="Print Report" />
+      {showSection && (
+        <div>
+          Link :<a href="https://github.com/ismaiel5/react-challenge/commits/master"> Click to download objects</a>
+          <br />
+          <br />
+          <Button title="Print Report" />
+        </div>
+      )}
     </section>
   );
 };
