f494b2e9459e4194d5d52cdd5b3cb46ee94eee21
ismaiel <ismaiel.altahan@gmail.com>
2021-06-19 18:55:32 +0800

    added reportCard component

---
+++ components/ReportCard.js
@@ -0,0 +1,9 @@
+import Styles from "../styles/Card.module.scss";
+
+const ReportCard = (props) => {
+  return (
+  <section className={Styles.card}>report</section>
+  )
+};
+
+export default ReportCard;
