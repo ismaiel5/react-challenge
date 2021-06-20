d8c8549f52507e725f3690cd1637fb31bb24d683
ismaiel <ismaiel.altahan@gmail.com>
2021-06-20 12:05:28 +0800

    update the report card component to use the data via props and display them properly in the card

--- components/ReportCard.js
+++ components/ReportCard.js
@@ -1,9 +1,20 @@
 import Styles from "../styles/Card.module.scss";
 
 const ReportCard = (props) => {
+  const objectList = props.objects.map((obj, index) => {
+    return (
+      <li key={index}>
+        {" "}
+        {Object.keys(obj)} : {Object.values(obj)}{" "}
+      </li>
+    );
+  });
+
   return (
-  <section className={Styles.card}>report</section>
-  )
+    <section className={Styles.card}>
+      <p>{objectList}</p>
+    </section>
+  );
 };
 
 export default ReportCard;
